3d0345ad5061fa4910ae9e95099f0a463598054a
Ben Gottlieb <ben@standalone.com>
2014-06-05 06:13:07 -0500

    use proper Alert class on iOS 8 (UIAlertController)

--- StackWatcher/Classes/AlertManager.swift
+++ StackWatcher/Classes/AlertManager.swift
@@ -25,41 +25,38 @@ class AlertManager : NSObject, UIAlertViewDelegate {
 	}
 
 	var alertCompletionBlocks: Dictionary<Int, (buttonIndex: Int) -> ()> = [:]
-
+	
 	func showAlertTitled(title: NSString, message: NSString, buttonTitles: String[], completion: (buttonIndex: Int) -> () = {(buttonIndex: Int) -> () in }) {
-		var alertView = UIAlertView()
-		alertView.title = title
-		alertView.message = message
-		for button in buttonTitles {
-			alertView.addButtonWithTitle(button)
+		
+		var alertController: AnyClass? = NSClassFromString("UIAlertController")
+		
+		if let controller: AnyClass = alertController {
+			var alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+			
+			for index in 0..buttonTitles.count {
+				alert.addAction(UIAlertAction(title: buttonTitles[index], style: (index == 0) ? UIAlertActionStyle.Cancel :  UIAlertActionStyle.Default, handler: {(alert :UIAlertAction!) -> Void in
+					completion(buttonIndex: index)
+					}))
+			}
+			
+			UIApplication.sharedApplication().delegate?.window?.rootViewController.presentViewController(alert, animated: true, completion: nil)
+		} else {
+			var alertView = UIAlertView()
+			alertView.title = title
+			alertView.message = message
+			for button in buttonTitles {
+				alertView.addButtonWithTitle(button)
+			}
+			alertView.cancelButtonIndex = 0
+			alertView.tag = self.alertCompletionBlocks.count
+			self.alertCompletionBlocks[alertView.tag] = completion
+			
+			alertView.delegate = self
+			alertView.show()
 		}
-		alertView.cancelButtonIndex = 0
-		alertView.tag = self.alertCompletionBlocks.count
-		self.alertCompletionBlocks[alertView.tag] = completion
-		
-		alertView.delegate = self
-		alertView.show()
-		
-		println("Alert delegate: \(alertView.delegate), blocks: \(self.alertCompletionBlocks)")
 	}
 	
 	func alertView(alertView: UIAlertView!, clickedButtonAtIndex buttonIndex: Int) {
 		self.alertCompletionBlocks[alertView.tag]?(buttonIndex: buttonIndex)
 	}
 }
-
-
-
-
-//			var alert = UIAlertController(title: "Not Authorized Yet", message: "You've not yet authorized with Stack Exchange. Would you like to do so now?", preferredStyle: .Alert)
-//
-//			alert.addAction(UIAlertAction(title: "Authorize", style: UIAlertActionStyle.Default, handler: {(alert :UIAlertAction!) -> Void in
-//				self.authenticate()
-//				}))
-//
-//
-//			alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: {(alert :UIAlertAction!) -> Void in
-//
-//				}))
-//
-//			self.presentViewController(alert, animated: true, completion: nil)
