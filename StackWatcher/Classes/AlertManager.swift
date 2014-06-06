125ec71cb7af31585d23a8d56dc84fc8d9663483
Ben Gottlieb <ben@standalone.com>
2014-06-05 22:16:36 -0500

    alerts now fire on the main thread

--- StackWatcher/Classes/AlertManager.swift
+++ StackWatcher/Classes/AlertManager.swift
@@ -20,32 +20,33 @@ class AlertManager : NSObject, UIAlertViewDelegate {
 	var alertCompletionBlocks: Dictionary<Int, (buttonIndex: Int) -> ()> = [:]
 	
 	func showAlertTitled(title: NSString, message: NSString, buttonTitles: String[], completion: (buttonIndex: Int) -> () = {(buttonIndex: Int) -> () in }) {
-		
-		var alertController: AnyClass? = NSClassFromString("UIAlertController")
-		
-		if let controller: AnyClass = alertController {
-			var alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+		dispatch_async(dispatch_get_main_queue()) {
+			var alertController: AnyClass? = NSClassFromString("UIAlertController")
 			
-			for (index, title) in enumerate(buttonTitles) {
-				alert.addAction(UIAlertAction(title: title, style: (index == 0) ? UIAlertActionStyle.Cancel :  UIAlertActionStyle.Default, handler: {(alert :UIAlertAction!) -> Void in
-					completion(buttonIndex: index)
-				}))
+			if let controller: AnyClass = alertController {
+				var alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+				
+				for (index, title) in enumerate(buttonTitles) {
+					alert.addAction(UIAlertAction(title: title, style: (index == 0) ? UIAlertActionStyle.Cancel :  UIAlertActionStyle.Default, handler: {(alert :UIAlertAction!) -> Void in
+						completion(buttonIndex: index)
+					}))
+				}
+				
+				UIApplication.sharedApplication().delegate?.window?.rootViewController.presentViewController(alert, animated: true, completion: nil)
+			} else {
+				var alertView = UIAlertView()
+				alertView.title = title
+				alertView.message = message
+				for button in buttonTitles {
+					alertView.addButtonWithTitle(button)
+				}
+				alertView.cancelButtonIndex = 0
+				alertView.tag = self.alertCompletionBlocks.count
+				self.alertCompletionBlocks[alertView.tag] = completion
+				
+				alertView.delegate = self
+				alertView.show()
 			}
-			
-			UIApplication.sharedApplication().delegate?.window?.rootViewController.presentViewController(alert, animated: true, completion: nil)
-		} else {
-			var alertView = UIAlertView()
-			alertView.title = title
-			alertView.message = message
-			for button in buttonTitles {
-				alertView.addButtonWithTitle(button)
-			}
-			alertView.cancelButtonIndex = 0
-			alertView.tag = self.alertCompletionBlocks.count
-			self.alertCompletionBlocks[alertView.tag] = completion
-			
-			alertView.delegate = self
-			alertView.show()
 		}
 	}
 	
