17ac1274494fa66c64b7a880ba5ff915ea3ecb5f
Ben Gottlieb <ben@standalone.com>
2014-06-09 06:10:49 -0500

    added datasource

--- StackWatcher Mac/Controllers/Questions List/QuestionsListController.swift
+++ StackWatcher Mac/Controllers/Questions List/QuestionsListController.swift
@@ -9,19 +9,58 @@
 import Cocoa
 import WebKit
 
-class QuestionsListController: NSWindowController {
+class QuestionsListController: NSWindowController, NSTableViewDataSource {
 	@IBOutlet var webView: WebView
 	@IBOutlet var splitView: NSSplitView
+	@IBOutlet var questionsTable: NSTableView
+	var fetchRequest = NSFetchRequest(entityName: "PostedQuestion")
+	
+	var questions = []
+	var searchTag = "swift-language"
 	
 	
 	class func controller() -> (QuestionsListController) {
 		var controller = QuestionsListController(windowNibName: "QuestionsListController")
 		
+		controller.fetchRequest.sortDescriptors = [ NSSortDescriptor(key: "last_activity_date", ascending: false) ]
+		
 		controller.showWindow(nil)
 		
 		controller.window.makeKeyAndOrderFront(nil)
-		
+		NSNotificationCenter.defaultCenter().addObserver(controller, selector: "updateQuestions", name: StackInterface.DefaultInterface.didAuthenticateNotificationName, object: nil)
+
 		return controller
 	}
+	
+	override func windowWillLoad()  {
+		self.updateQuestions()
+	}
+	
+}
+
+extension QuestionsListController {
+	func updateQuestions() {
+		if !StackInterface.DefaultInterface.isAuthorized {
+			(NSApplication.sharedApplication().delegate as AppDelegate).promptForAuthorization()
+			return
+		}
+		
+		StackInterface.DefaultInterface.fetchQuestionsForTag(self.searchTag, completion: {(error: NSError?) -> Void in
+			dispatch_async(dispatch_get_main_queue()) {
+				var error: NSError?
+				
+				self.questions = Store.DefaultStore.mainQueueContext.executeFetchRequest(self.fetchRequest, error: &error)
+				self.reloadQuestions()
+			}
+		})
+	}
+	
+	func reloadQuestions() {
+		
+	}
+}
+
+extension QuestionsListController {
+	func numberOfRowsInTableView(tableView: NSTableView!) -> Int { println("\(self.questions.count) questions found"); return self.questions.count }
 	
 }
